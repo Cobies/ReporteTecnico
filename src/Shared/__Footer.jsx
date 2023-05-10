6b1b1aef95fed09c49d8dd376b3e47bb43957d27
STING <STING@DESKTOP-HG3C72H>
2023-05-10 18:54:32 -0500

    Integre layaout

---
+++ src/Shared/__Footer.jsx
@@ -0,0 +1,16 @@
+
+const __Footer = () => {
+
+  return (
+
+    <footer
+      className="text-center text-lg-start text-white"
+      style={{ backgroundColor: "GREY" }}
+    >
+      HOLAAAAAAA
+    </footer>
+
+  );
+};
+
+export default __Footer;
