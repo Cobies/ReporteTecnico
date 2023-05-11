f19adcea61b173a9f23f5496f96acb8815c20810
STING <STING@DESKTOP-HG3C72H>
2023-05-11 11:42:30 -0500

    bug de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,10 @@ function App() {
     <BrowserRouter>
       <__Layout session={session} setSession={setSession} >
         <Routes>
-          <Route index path="/" element={<h1>Cliente</h1>} />
+
           <Route index path="/admin" element={<h1>EMPLEADO</h1>} />
           <Route exact element={<ProtectedRoutes session={session} />}>
+            <Route index path="/" element={<h1 className="container">Cliente</h1>} />
             <Route path="/profile" element={<h1>PERFIL</h1>} />
           </Route>
           <Route path="*" element={<Error />} />
