d670d50d271386d3ee314717256645780106209d
Cobies <fagp2014@gmail.com>
2023-05-12 09:43:04 -0500

    correccion

--- src/Models/ReporteTecnico/UsuarioClienteReporteTecnico.jsx
+++ src/Models/ReporteTecnico/UsuarioClienteReporteTecnico.jsx
@@ -1,11 +1,10 @@
 import Cliente from "../Persona/Cliente";
-import html2pdf from 'html2pdf.js';
-import Vista from "./Vista";
 
-    const UsuarioClienteReporteTecnico = {
-        _id: "",
-        fechaCreado: new Date(),
-        cliente: Cliente
-    }
+
+const UsuarioClienteReporteTecnico = {
+    _id: "",
+    fechaCreado: new Date(),
+    cliente: Cliente
+}
 
 export default UsuarioClienteReporteTecnico;
